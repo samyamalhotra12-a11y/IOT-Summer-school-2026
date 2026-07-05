174fd71f0061a6617662f06fdb4570746bfd599e
samyamalhotra12-a11y <samyamalhotra12@gmail.com>
2026-07-05 22:43:16 +0530

    docs: code uploaded

---
+++ ASSIGNMENTS/iot assignment 3/Q35_weather_data_fetcher/weather_data_fetcher.ino
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* WIFI_SSID = "Your_WiFi_Name";
+const char* WIFI_PASSWORD = "Your_WiFi_Password";
+
+const char* API_KEY = "YOUR_API_KEY";
+
+const char* CITY = "Jammu";     
+const char* COUNTRY = "IN";
+
+#endif
